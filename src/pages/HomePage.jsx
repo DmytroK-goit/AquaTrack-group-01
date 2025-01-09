02db98bb242574268b4faeb99804fadc7f517fbf
vitalii-pc <alievruslan1818@gmail.com>
2025-01-09 23:02:22 +0100

    USM_first_commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,15 @@
 import AdvantagesSection from "../components/HomePage/HomePageComponents/AdvantagesSection";
 import WelcomeSection from "../components/HomePage/HomePageComponents/WelcomeSection";
+import UserSettingsModal from "../components/UserSettingsModal/UserSettingsModal.jsx";
 
 const HomePage = () => {
-  return (
-    <div className="flex justify-between">
-      <WelcomeSection />
-      <AdvantagesSection />
-    </div>
-  );
+	return (
+		<div className="flex justify-between">
+			<WelcomeSection />
+			<AdvantagesSection />
+			<UserSettingsModal />
+		</div>
+	);
 };
 
 export default HomePage;
