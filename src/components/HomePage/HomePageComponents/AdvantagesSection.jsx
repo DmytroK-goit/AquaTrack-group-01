a880a59b566b8c3fb14c7f4192b9b6f6b6ce0a1c
Iryna Oliinyk <arairishka2@gmail.com>
2025-01-09 22:50:29 +0100

    WelcomeSection +AdvantagesSection_mobil

--- src/components/HomePage/HomePageComponents/AdvantagesSection.jsx
+++ src/components/HomePage/HomePageComponents/AdvantagesSection.jsx
@@ -1,7 +1,11 @@
+import css from "./AdvantagesSection.module.css";
+
 const AdvantagesSection = () => {
   return (
-    <div className="w-96 h-96 border-2 border-black	rounded-md bg-slate-100">
-      <h1>IMG</h1>
+    <div
+      className={`${css.conteiner} w-96 h-96 border-2 border-black	rounded-md bg-slate-100`}
+    >
+      <h1 className={css.background}></h1>
     </div>
   );
 };
