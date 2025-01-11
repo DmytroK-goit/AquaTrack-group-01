8b71fd7895fa7b1fcd70a2f8cc8b2f5e531430db
Dmytro Kovbasiuk <k0vbasyuk.dim0n@gmail.com>
2025-01-11 18:54:34 +0200

    add refresh

--- src/redux/UserAuth/selectors.js
+++ src/redux/UserAuth/selectors.js
@@ -1,4 +1,5 @@
 export const selectUser = (state) => state.auth.user;
+export const selectToken = (state) => state.auth.token;
 export const selectUserName = (state) => state.auth.user?.name || "User";
 export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
 export const selectIsRefreshing = (state) => state.auth.isRefreshing;
