cfb8a55be5573f2ae2214cce174e00a3c735352a
kenyafan <kenyafan321@gmail.com>
2025-01-12 19:54:06 +0200

    close x

--- src/components/TrackerPage/modal/Modal/Modal.jsx
+++ src/components/TrackerPage/modal/Modal/Modal.jsx
@@ -13,6 +13,21 @@ export default function Modal({ isOpen, onClose, children }) {
   return (
     <div className={css.overlay} onClick={handleOverlayClick}>
       <div ref={modalRef} className={css.modal}>
+        <button className={css.closeButton} onClick={onClose}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-x"
+          >
+            <line x1="18" y1="6" x2="6" y2="18" />
+            <line x1="6" y1="6" x2="18" y2="18" />
+          </svg>
+        </button>
         {children}
       </div>
     </div>
