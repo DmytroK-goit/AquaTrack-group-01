806baf90f7a03445e9bc473bbc7241484c708c8f
Dmytro Kovbasiuk <k0vbasyuk.dim0n@gmail.com>
2025-01-12 11:28:17 +0200

    logout

--- src/components/TrackerPage/WaterDetails/RightPart/UserBar/UserBarPopover/UserBarPopover.jsx
+++ src/components/TrackerPage/WaterDetails/RightPart/UserBar/UserBarPopover/UserBarPopover.jsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
 import s from "./UserBarPopover.module.css";
+import UserSettingsForm from "../../../../modal/UserSettingsForm/UserSettingsForm";
+import { useDispatch } from "react-redux";
+import { logout } from "../../../../../../redux/UserAuth/operations";
 
 const UserBarPopover = () => {
+  const [isOpenSetting, setIsOpenSetting] = useState(false);
+
+  const handleSettingClick = () => {
+    setIsOpenSetting(true);
+  };
+
+  const handleCloseSetting = () => {
+    setIsOpenSetting(false);
+  };
+
+  const dispatch = useDispatch();
+  const handleLogOut = () => {
+    dispatch(logout());
+  };
   return (
     <div className={s.popover}>
-      <button className={s.popoverButtonSettings}>Settings</button>
-      <button className={s.popoverButtonLogOut}>Log out</button>
+      <button onClick={handleSettingClick} className={s.popoverButtonSettings}>
+        Settings
+      </button>
+      <button onClick={handleLogOut} className={s.popoverButtonLogOut}>
+        Log out
+      </button>
+      {isOpenSetting && <UserSettingsForm onClose={handleCloseSetting} />}
     </div>
   );
 };
