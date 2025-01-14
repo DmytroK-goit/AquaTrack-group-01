3445f265666f41dd71c278675a62825319861c47
chasov-artem <chasov90@gmail.com>
2025-01-14 10:19:41 +0200

    media-styles

--- src/components/TrackerPage/WaterDetails/RightPart/UserBar/UserBarPopover/UserBarPopover.jsx
+++ src/components/TrackerPage/WaterDetails/RightPart/UserBar/UserBarPopover/UserBarPopover.jsx
@@ -1,13 +1,10 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { logout } from "../../../../../../redux/UserAuth/operations";
 import Modal from "../../../../modal/Modal/Modal";
 import LogOutModal from "../../../../modal/LogOutModal/LogOutModal";
 import s from "./UserBarPopover.module.css";
 import UserSettingsModal from "../../../../modal/UserSettingsModal/UserSettingsModal";
 
 const UserBarPopover = () => {
-  const dispatch = useDispatch();
   const [isLogOutModalOpen, setIsLogOutModalOpen] = useState(false);
   const [isOpenSetting, setIsOpenSetting] = useState(false);
   const [isPopoverOpen, setIsPopoverOpen] = useState(true);
