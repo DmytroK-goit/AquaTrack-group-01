b2f168c64184c386e6fec1065a72c601f5736d2b
Iryna Oliinyk <arairishka2@gmail.com>
2025-01-20 17:21:58 +0100

    edits

--- src/components/HomePage/HomePageComponents/AdvantagesSection.jsx
+++ src/components/HomePage/HomePageComponents/AdvantagesSection.jsx
@@ -8,50 +8,50 @@ import avatar3_tab from "../../../images/AdvantagesSection/avatar3_tab.png";
 
 const AdvantagesSection = ({ userCount }) => {
   return (
-    <div className={css.conteiner}>
-      <div className={css.background}>
-        <div className={css.happyCustomers}>
-          <div className={css.conteinerAvatar}>
-            <img
-              src={avatar1_tab}
-              srcSet={`
+    // <div className={css.conteiner}>
+    <div className={css.background}>
+      <div className={css.happyCustomers}>
+        <div className={css.conteinerAvatar}>
+          <img
+            src={avatar1_tab}
+            srcSet={`
           ${avatar1} 375px, 
           ${avatar1_tab} 768px, 
           
         `}
-              alt="Avatar 1"
-              className={css.avatar}
-            />
-            <img
-              src={avatar2_tab}
-              srcSet={`
+            alt="Avatar 1"
+            className={css.avatar}
+          />
+          <img
+            src={avatar2_tab}
+            srcSet={`
           ${avatar2} 375px, 
           ${avatar2_tab} 768px, 
           
         `}
-              alt="Avatar 2"
-              className={css.avatar}
-            />
-            <img
-              src={avatar3_tab}
-              srcSet={`
+            alt="Avatar 2"
+            className={css.avatar}
+          />
+          <img
+            src={avatar3_tab}
+            srcSet={`
           ${avatar3} 375px, 
           ${avatar3_tab} 768px, 
           
         `}
-              alt="Avatar 3"
-              className={css.avatar}
-            />
-          </div>
-          <div className={css.conteinerSpan}>
-            <span>Our </span>
-            <span className={css.highlight}>happy </span>
-            <span> customers:</span>
-            {userCount}
-          </div>
+            alt="Avatar 3"
+            className={css.avatar}
+          />
+        </div>
+        <div className={css.conteinerSpan}>
+          <span>Our </span>
+          <span className={css.highlight}>happy </span>
+          <span> customers:</span>
+          {userCount}
         </div>
       </div>
     </div>
+    // </div>
   );
 };
 export default AdvantagesSection;
